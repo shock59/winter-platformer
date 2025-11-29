0dccdb29237158aa168b9181ac3803923f4ec017
shock59 <42130427+shock59@users.noreply.github.com>
2025-11-29 14:17:15 +1100

    Add canvas and main loop

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,33 @@
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = "Winter Platformer";
+class GameCanvas {
+  element: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+
+  constructor() {
+    this.element = document.createElement("canvas");
+    this.ctx = this.element.getContext("2d")!;
+
+    requestAnimationFrame(() => this.frame());
+  }
+
+  frame() {
+    this.element.width = window.innerWidth;
+    this.element.height = window.innerHeight;
+
+    this.ctx.reset();
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(
+      window.innerWidth / 2 - 50,
+      window.innerHeight / 2 - 50,
+      100,
+      100
+    );
+
+    requestAnimationFrame(() => this.frame());
+  }
+}
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+const gameCanvas = new GameCanvas();
+app.appendChild(gameCanvas.element);
